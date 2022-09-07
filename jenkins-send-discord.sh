d6c8b9d2cf44aece24819357cf775a85057d1354
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-09-07 19:30:39 +0700

    Add approval

--- jenkins-send-discord.sh
+++ jenkins-send-discord.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+jenkins_job_name="$(echo ${JOB_NAME} | awk -F / '{print $1}')"
+
+if [[ "${jenkins_notification_type}" == "approval" ]]; then
+  color="43775"
+  jenkins_job_status="WAITING_FOR_APPROVAL"
+  jenkins_job_url="${jenkins_blue_ocean_base_url}/${jenkins_job_name}/detail/${jenkins_branch_name}/${jenkins_job_number}/pipeline"
+else
+  if [[ "${jenkins_build_result}" == "SUCCESS" ]]; then
+    color="65344"
+  else
+    color="16711701"
+  fi
+fi
+
 date_time="$(date -u +%Y-%m-%dT%H:%M:%S.000Z)"
 
 json_data=$(cat <<EOF
