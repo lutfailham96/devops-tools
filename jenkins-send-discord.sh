9662f8d955bb9b4c78c0c27184a0e54e9bf375eb
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-09-08 16:07:37 +0700

    Update `jenkins-send-discord.sh`

--- jenkins-send-discord.sh
+++ jenkins-send-discord.sh
@@ -21,7 +21,8 @@ jenkins_job_name="$(echo ${JOB_NAME} | awk -F / '{print $1}')"
 
 if [ -n "${discord_id}" ]; then
   while IFS= read -r user; do
-    discord_mention+="<@${user}> "
+    #discord_mention+="<@${user}> "
+    discord_mention="${discord_mention} <@${user}>"
   done <<< $(echo "${discord_id}" | sed -n 1'p' | tr ',' '\n')
 fi
 
